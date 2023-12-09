5c1133372d3b888d129b2b26cd9459f9d1dcddc2
syed <syedabuubaid@gmail.com>
2023-12-10 01:08:53 +0530

    .

--- script.js
+++
@@ -1,5 +0,0 @@
-
-var color=["blue","purple","green"]
-docment.querySelector("div").addEventListener("mouseover",function(){
-    document.querySelector("div").stylebackground=color[Math.random()*color.length];
-})
